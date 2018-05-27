5711cce1560bd1cac2a639005240861221d74d88
Stephen F <stephen@farver.io>
2018-05-27 00:00:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,11 @@ def countdown(integer)
   while integer > 0 
     puts "#{integer} SECOND(S)!"
     integer -= 1 
-    sleep(1)
   end 
   puts "HAPPY NEW YEAR"
+end
+
+def countdown_with_sleep(integer)
+  countdown(integer)
+  sleep(1)
 end
